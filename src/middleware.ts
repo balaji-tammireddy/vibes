e693c2813dd256b58f00134864ef9afb8eeb1962
Tammireddy Balaji Ganesh <tammireddybalaji@gmail.com>
2025-08-10 19:36:36 +0530

    middleware added

---
+++ src/middleware.ts
@@ -0,0 +1,31 @@
+import { NextResponse, NextRequest } from "next/server";
+
+export function middleware(request: NextRequest) {
+  const path = request.nextUrl.pathname;
+
+  const isPublicPath = path === "/login" || path === "/register" || path === "/verify" || path === "/forgot-password" || path === "/forgot-password/verify" || path === "/forgot-password/reset";
+  const token = request.cookies.get("token")?.value;
+
+  if (isPublicPath && token) {
+    return NextResponse.redirect(new URL("/home", request.url));
+  }
+
+  if (!isPublicPath && !token) {
+    return NextResponse.redirect(new URL("/login", request.url));
+  }
+
+  return NextResponse.next();
+}
+
+export const config = {
+  matcher: [
+    "/home",
+    "/upload",
+    "/messages/:path*",
+    "/search/:path*",
+    "/profile/:path*",
+
+    "/login",
+    "/register",
+  ],
+};
