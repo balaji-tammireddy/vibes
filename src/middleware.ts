eda0926f273fb707d55f0cb08c96e93e09236dab
Tammireddy Balaji Ganesh <tammireddybalaji@gmail.com>
2025-08-15 03:15:36 +0530

    middleware update

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,35 +1,36 @@
-import { NextResponse, NextRequest } from "next/server";
+  import { NextResponse, NextRequest } from "next/server";
 
-export function middleware(request: NextRequest) {
-  const path = request.nextUrl.pathname;
+  export function middleware(request: NextRequest) {
+    const path = request.nextUrl.pathname;
 
-  const isPublicPath = path === "/login" || path === "/register" || path === "/verify" || path === "/forgot-password" || path === "/forgot-password/verify" || path === "/forgot-password/reset";
-  const token = request.cookies.get("token")?.value;
+    const isPublicPath = path === "/login" || path === "/register" || path === "/verify" || path === "/forgot-password" || path === "/forgot-password/verify" || path === "/forgot-password/reset";
+    const token = request.cookies.get("token")?.value;
 
-  if (isPublicPath && token) {
-    return NextResponse.redirect(new URL("/home", request.url));
+    if (isPublicPath && token) {
+      return NextResponse.redirect(new URL("/home", request.url));
+    }
+
+    if (!isPublicPath && !token) {
+      return NextResponse.redirect(new URL("/login", request.url));
+    }
+
+    return NextResponse.next();
   }
 
-  if (!isPublicPath && !token) {
-    return NextResponse.redirect(new URL("/login", request.url));
-  }
+  export const config = {
+    matcher: [
+      "/",
+      "/home",
+      "/upload",
+      "/messages/:path*",
+      "/search/:path*",
+      "/profile/:path*",
 
-  return NextResponse.next();
-}
-
-export const config = {
-  matcher: [
-    "/home",
-    "/upload",
-    "/messages/:path*",
-    "/search/:path*",
-    "/profile/:path*",
-
-    "/login",
-    "/register",
-    "/verify",
-    "/forgot-password",
-    "/forgot-password/verify",
-    "/forgot-password/reset",
-  ],
-};
+      "/login",
+      "/register",
+      "/verify",
+      "/forgot-password",
+      "/forgot-password/verify",
+      "/forgot-password/reset",
+    ],
+  };
